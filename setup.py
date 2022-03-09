60fce2ab0a861b6399a4dd1c11a86e6a3a7fbb7b
J-81 <jonathan.d.oribello@gmail.com>
2022-03-08 18:10:57 -0800

    Adding setup tools

---
+++ setup.py
@@ -0,0 +1,24 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+dirname = os.path.dirname(__file__)
+
+long_description = (
+    codecs.open(os.path.join(dirname, 'README.md'), encoding='utf-8').read() + '\n' +
+    codecs.open(os.path.join(dirname, 'CHANGELOG.md'), encoding='utf-8').read()
+)
+
+setup(
+   name='dp_tools',
+   version='0.1.0',
+   description='Tooling for Data Processing Operations',
+   author='Jonathan Oribello',
+   author_email='jonathan.d.oribello@nasa.gov',
+   packages=find_packages(),  #same as name
+   scripts=[],
+   python_requires='>=3.10',
+   install_requires=[],
+   setup_requires=[],
+   tests_require=[]
+)
