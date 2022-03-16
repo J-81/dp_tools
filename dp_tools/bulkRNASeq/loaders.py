1ef82873e08954337ad874dc5d02120f167bcb7b
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-03-16 09:58:53 -0700

    Added in stage 01 loader function. Added in stack system for staged loading.

--- dp_tools/bulkRNASeq/loaders.py
+++ dp_tools/bulkRNASeq/loaders.py
@@ -11,7 +11,7 @@ from typing import List, Protocol, Union
 
 import pandas as pd
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s')
 log = logging.getLogger(__name__)
 
 from dp_tools.core.entity_model import (
@@ -22,14 +22,15 @@ from dp_tools.core.entity_model import (
     DataDir,
     DataFile,
     GLDSDataSystem,
+    TemplateDataSystem,
 )
 
 from dp_tools.bulkRNASeq.entity import BulkRNASeqDataset, BulkRNASeqSample
-from dp_tools.bulkRNASeq.locaters import FastqcReport, MultiQCDir, RawFastq, Runsheet
+from dp_tools.bulkRNASeq.locaters import FastqcReport, MultiQCDir, Fastq, Runsheet
 from dp_tools.components import RawReadsComponent, BulkRNASeqMetadataComponent
 
 
-def load_from_bulk_rnaseq_raw_dir(root_path: Path, dataSystem_name: str = None):
+def load_BulkRNASeq_STAGE_00(root_path: Path, dataSystem_name: str = None, stack: bool = False):
     # ensure root path exists!
     if not root_path.is_dir():
         raise FileNotFoundError(f"Root path doesn't exist!: {root_path}")
@@ -42,7 +43,7 @@ def load_from_bulk_rnaseq_raw_dir(root_path: Path, dataSystem_name: str = None):
     dataSystem = GLDSDataSystem(base=BaseDataSystem(name=dataSystem_name))
 
     # initate locaters on the root path
-    rawFastq = RawFastq(search_root=root_path)
+    rawFastq = Fastq(search_root=root_path)
     runsheet = Runsheet(search_root=root_path)
     readsMQC = MultiQCDir(search_root=root_path)
     fastqcReport = FastqcReport(search_root=root_path)
@@ -106,5 +107,91 @@ def load_from_bulk_rnaseq_raw_dir(root_path: Path, dataSystem_name: str = None):
             sample.attach_component(raw_reads, attr="rawReads")
         # attach components
         dataset.attach_sample(sample)
+    
+    # return dataSystem only if not being used in a loading stack
+    if stack:
+        return root_path, dataSystem
+    else:
+        return dataSystem
 
-    return dataSystem
+# TODO: docstring
+def load_BulkRNASeq_STAGE_01(root_path: Path, dataSystem: TemplateDataSystem, stack: bool = False):
+    """Load data that should be present into stage 01 (TG-PreProc)
+
+    :param dataSystem: The dataSystem as loaded through STAGE 00
+    :type dataSystem: TemplateDataSystem
+    """
+     # ensure root path exists!
+    if not root_path.is_dir():
+        raise FileNotFoundError(f"Root path doesn't exist!: {root_path}")
+
+    # log about datasystem being built upon
+    log.info(f"Loading STAGE 01 BulkRNASeq data into: \n\t{dataSystem}")
+
+    # initate locaters on the root path
+    fastq = Fastq(search_root=root_path)
+    readsMQC = MultiQCDir(search_root=root_path)
+    fastqcReport = FastqcReport(search_root=root_path)
+
+    # attach dataset components
+    # dataSystem.dataset.attach_component(
+    #     BulkRNASeqMetadataComponent(
+    #         base=BaseComponent(description="Metadata in a runsheet csv file"),
+    #         runsheet=DataFile(runsheet.find(datasystem_name=dataSystem_name)),
+    #     ),
+    #     attr="metadata",
+    # )
+
+    # alias for convenience
+    metadata = dataSystem.dataset.all_components["metadata"]
+    dataset = dataSystem.dataset
+
+    # create shared sample datafiles
+    datf_readsMQC = DataDir(
+        readsMQC.find(
+            rel_dir=Path.joinpath(Path("01-TG_Preproc"), Path("FastQC_Reports")),
+            mqc_label="trimmed",
+        )
+    )
+
+    # update samples
+    for sample_name, sample in dataset.samples.items():
+        if metadata.paired_end:
+            _type = ("Trimmed", "Forward")
+            trim_fwd_reads = RawReadsComponent(
+                base=BaseComponent(description="Trimmed Forward Reads"),
+                fastqGZ=DataFile(fastq.find(sample_name, type=_type)),
+                multiQCDir=datf_readsMQC,
+                fastqcReportHTML=DataFile(path=fastqcReport.find(sample_name=sample_name, ext="html", type=_type)),
+                fastqcReportZIP=DataFile(path=fastqcReport.find(sample_name=sample_name, ext="zip", type=_type))
+            )
+            _type = ("Trimmed", "Reverse")
+            trim_rev_reads = RawReadsComponent(
+                base=BaseComponent(description="Trimmed Reverse Reads"),
+                fastqGZ=DataFile(
+                    path=fastq.find(sample_name, type=_type)
+                ),
+                multiQCDir=datf_readsMQC,
+                fastqcReportHTML=DataFile(path=fastqcReport.find(sample_name=sample_name, ext="html", type=_type)),
+                fastqcReportZIP=DataFile(path=fastqcReport.find(sample_name=sample_name, ext="zip", type=_type))
+            )
+            sample.attach_component(trim_fwd_reads, attr="trimForwardReads")
+            sample.attach_component(trim_rev_reads, attr="trimReverseReads")
+        else:
+            _type = ("Trimmed", "Forward")
+            trim_reads = RawReadsComponent(
+                base=BaseComponent(description="Trimmed Reads"),
+                fastqGZ=DataFile(
+                    path=fastq.find(sample_name, type=_type)
+                ),
+                multiQCDir=datf_readsMQC,
+                fastqcReportHTML=DataFile(path=fastqcReport.find(sample_name=sample_name, ext="html", type=_type)),
+                fastqcReportZIP=DataFile(path=fastqcReport.find(sample_name=sample_name, ext="zip", type=_type))
+            )
+            sample.attach_component(trim_reads, attr="trimReads")
+    
+    # return dataSystem only if not being used in a loading stack
+    if stack:
+        return root_path, dataSystem
+    else:
+        return dataSystem
