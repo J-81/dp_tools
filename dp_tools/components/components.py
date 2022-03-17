785f53a7ce2e0993b6d4ee7052d24941a3ed2038
J-81 <jonathan.d.oribello@gmail.com>
2022-03-17 16:30:13 -0700

    Expanded mqc auto-extraction. Including general stats. Plots data next. Will abstract this to TemplateComponent eventually for reuse.

--- dp_tools/components/components.py
+++ dp_tools/components/components.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, OrderedDict, Union
 
 import logging
 
@@ -17,22 +17,79 @@ from dp_tools.core.entity_model import (
 )
 from dp_tools.core.utilites.multiqc_tools import format_as_dataframe, get_general_stats
 
+# type hint definitions
+SampleLevelDict = OrderedDict[  # samples level
+    "str",  # denotes sample name
+    OrderedDict["str", float],  # key:value level, e.g. 'percent_gc': 52.0
+]
+
+GeneralStatsDict = Dict["str", SampleLevelDict]  # modules level  # denotes module name
+
+GeneralStatsDictForComponent = OrderedDict[  # samples level
+    "str",  # denotes sample name
+    OrderedDict["str", float],  # key:value level, e.g. 'percent_gc': 52.0
+]
+
+
 class ReadsComponent(TemplateComponent):
-    _count: int
+    _total_sequences: int
     # TODO: refine this type hint
     _mqc_data: dict
+    _mqc_modules_limit: List[str] = [
+        "FastQC"
+    ]  # optional but speeds up multiqc processing time slightly and can constrain modules if desired
+
+    def extract_mqc(self):
+        """Extracts multiQC data which exposes attributes for each general stat as well as plot data as separate attributes."""
+        self.mqc_general_stats
+        # self.mqc_plot_data # TODO: Implement
 
     @property
-    def count(self):
-        if not getattr(self,'_count'):
-            self._count = self.mqc_data
-        return self._count
-    
-    @property
     def mqc_general_stats(self) -> Dict:
-        if not getattr(self,'_mqc_data'):
-            self._mqc_data = get_general_stats(multiqc.run(analysis_dir = [self.fastqcReportZIP.path], no_report=True, no_data_dir=True, plots_interactive=True))
-        return self._mqc_data
+        if not getattr(self, "_mqc_general_stats", None):
+            general_stats_dict: GeneralStatsDict = get_general_stats(
+                multiqc.run(
+                    analysis_dir=[self.fastqcReportZIP.path],
+                    no_report=True,
+                    no_data_dir=True,
+                    plots_interactive=True,  # ensure data is robustly populated (otherwise flat plots result in missing extractable data)
+                    module=[
+                        module.lower() for module in self._mqc_modules_limit
+                    ],  # module names here are always lowercase
+                )
+            )
+            # assert only one sample
+            # then remove sample layer (but keep module layer for recognizing source multiqc parser)
+            general_stats_for_this_component: GeneralStatsDictForComponent = dict()  # type: ignore
+            for module in general_stats_dict:
+                # assert only one sample
+                assert (
+                    len(general_stats_dict[module]) == 1
+                ), "One and only one sample expected"
+                # extract the one key,value dict and remove the sample layer by assignment overwrite
+                general_stats_for_this_component[module] = list(
+                    general_stats_dict[module].values()
+                )[0]
+
+                # set individual attributes
+                for key, value in general_stats_for_this_component[module].items():
+                    # check if this clashes with anything else
+                    if getattr(self, key, None):
+                        new_key = f"{module}_{key}"
+                        log.warning(
+                            f"Attribute: '{key}' already exists.  Prepending module to disambiguate: '{new_key}'"
+                        )
+                        key = new_key
+                    log.debug(
+                        f"Extracting {key} from multiQC module {module} into {self}"
+                    )
+                    setattr(self, key, value)
+
+            # set whole dict here
+            self._mqc_general_stats = general_stats_for_this_component
+
+        return self._mqc_general_stats
+
 
 @dataclass(eq=False)
 class RawReadsComponent(ReadsComponent):
