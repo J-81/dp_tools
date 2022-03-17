6994959bb847075ba9c56ed753e6e1f667557186
J-81 <jonathan.d.oribello@gmail.com>
2022-03-17 14:00:25 -0700

    Cleanup. Using fixtures where appropriate. Standardized test names

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,108 +1,163 @@
 from pathlib import Path
 import os
 
-from dp_tools.bulkRNASeq.loaders import load_BulkRNASeq_STAGE_00, load_BulkRNASeq_STAGE_01
+from dp_tools.bulkRNASeq.loaders import (
+    load_BulkRNASeq_STAGE_00,
+    load_BulkRNASeq_STAGE_01,
+)
 import pytest
 
 # set for testing
 TEST_DIR = Path(os.environ["TEST_ASSETS_DIR"])
 
-def test_from_bulk_rnaseq_with_bad_rootdir():
-    target_data_dir = TEST_DIR / "GLDS-48_BUTWITHTYPOS"
+
+@pytest.fixture
+def glds194_sample_names():
+    return [
+        "Mmus_BAL-TAL_LRTN_BSL_Rep1_B7",
+        "Mmus_BAL-TAL_RRTN_BSL_Rep2_B8",
+        "Mmus_BAL-TAL_RRTN_BSL_Rep3_B9",
+        "Mmus_BAL-TAL_RRTN_BSL_Rep4_B10",
+        "Mmus_BAL-TAL_LRTN_GC_Rep1_G6",
+        "Mmus_BAL-TAL_LRTN_GC_Rep2_G8",
+        "Mmus_BAL-TAL_LRTN_GC_Rep3_G9",
+        "Mmus_BAL-TAL_RRTN_GC_Rep4_G10",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep1_F6",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep2_F7",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep3_F8",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep4_F9",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep5_F10",
+    ]
+
+
+@pytest.fixture
+def glds48_sample_names():
+    return [
+        "Mmus_C57-6J_LVR_GC_I_Rep1_M31",
+        "Mmus_C57-6J_LVR_GC_I_Rep2_M32",
+        "Mmus_C57-6J_LVR_FLT_I_Rep1_M21",
+        "Mmus_C57-6J_LVR_FLT_I_Rep2_M22",
+        "Mmus_C57-6J_LVR_GC_C_Rep1_M36",
+        "Mmus_C57-6J_LVR_GC_C_Rep2_M37",
+        "Mmus_C57-6J_LVR_GC_C_Rep3_M38",
+        "Mmus_C57-6J_LVR_GC_C_Rep4_M39",
+        "Mmus_C57-6J_LVR_GC_C_Rep5_M40",
+        "Mmus_C57-6J_LVR_FLT_C_Rep1_M25",
+        "Mmus_C57-6J_LVR_FLT_C_Rep2_M26",
+        "Mmus_C57-6J_LVR_FLT_C_Rep3_M27",
+        "Mmus_C57-6J_LVR_FLT_C_Rep4_M28",
+        "Mmus_C57-6J_LVR_FLT_C_Rep5_M30",
+    ]
+
+
+@pytest.fixture
+def glds194_test_dir():
+    return TEST_DIR / "GLDS-194_TruncatedProcessed"
+
+
+@pytest.fixture
+def glds48_test_dir():
+    return TEST_DIR / "GLDS-48_TruncatedProcessed"
+
+
+@pytest.fixture
+def typo_test_dir():
+    return TEST_DIR / "GLDS-48_BUTWITHTYPOS"
+
+
+def test_bulk_rnaseq_with_bad_rootdir(typo_test_dir):
 
     with pytest.raises(FileNotFoundError):
-        ds = load_BulkRNASeq_STAGE_00(
-            target_data_dir,
-            dataSystem_name = "GLDS-48"
+        ds = load_BulkRNASeq_STAGE_00(typo_test_dir, dataSystem_name="GLDS-48")
+
+
+def test_bulk_rnaseq_STAGE00_single(caplog, glds48_test_dir, glds48_sample_names):
+    """ Tests loader for state after demultiplexing for single end study """
+
+    caplog.set_level(0)
+    ds = load_BulkRNASeq_STAGE_00(glds48_test_dir, dataSystem_name="GLDS-48")
+
+    # pull dataset
+    dataset = ds.datasets["GLDS-48__BulkRNASeq"]
+
+    assert list(dataset.samples.keys()) == glds48_sample_names
+
+    # check expected loaded components [raw directory]
+    for sample in ds.dataset.samples.values():
+        assert len(list(sample.rawReads.multiQCDir.path.iterdir())) == 2
+
+
+def test_bulk_rnaseq_STAGE00_paired(caplog, glds194_test_dir, glds194_sample_names):
+    """ Tests loader for state after demultiplexing for single end study """
+
+    caplog.set_level(0)
+    ds = load_BulkRNASeq_STAGE_00(glds194_test_dir, dataSystem_name="GLDS-194")
+
+    # pull dataset
+    dataset = ds.datasets["GLDS-194__BulkRNASeq"]
+
+    assert list(dataset.samples) == glds194_sample_names
+
+    # check expected loaded components [raw directory]
+    for sample in ds.dataset.samples.values():
+        assert len(list(sample.rawForwardReads.multiQCDir.path.iterdir())) == 2
+        assert len(list(sample.rawReverseReads.multiQCDir.path.iterdir())) == 2
+
+
+def test_bulk_rnaseq_STAGE01_single(caplog, glds48_test_dir, glds48_sample_names):
+    """ Tests loader for state after demultiplexing for single end study """
+
+    caplog.set_level(0)
+    ds = load_BulkRNASeq_STAGE_01(
+        *load_BulkRNASeq_STAGE_00(
+            glds48_test_dir, dataSystem_name="GLDS-48", stack=True
         )
-    
-
-def test_from_bulk_rnaseq_raw_dir_single(caplog):
-    """ Tests loader for state after demultiplexing for single end study """
-    target_data_dir = TEST_DIR / "GLDS-48_PostDemultiplex"
-
-    caplog.set_level(0)
-    ds = load_BulkRNASeq_STAGE_00(
-        target_data_dir,
-        dataSystem_name = "GLDS-48"
     )
 
     # pull dataset
     dataset = ds.datasets["GLDS-48__BulkRNASeq"]
 
-    assert list(dataset.samples.keys()) == [
-        'Mmus_C57-6J_LVR_GC_I_Rep1_M31', 'Mmus_C57-6J_LVR_GC_I_Rep2_M32', 'Mmus_C57-6J_LVR_FLT_I_Rep1_M21', 'Mmus_C57-6J_LVR_FLT_I_Rep2_M22', 'Mmus_C57-6J_LVR_GC_C_Rep1_M36', 'Mmus_C57-6J_LVR_GC_C_Rep2_M37', 'Mmus_C57-6J_LVR_GC_C_Rep3_M38', 'Mmus_C57-6J_LVR_GC_C_Rep4_M39', 'Mmus_C57-6J_LVR_GC_C_Rep5_M40', 'Mmus_C57-6J_LVR_FLT_C_Rep1_M25', 'Mmus_C57-6J_LVR_FLT_C_Rep2_M26', 'Mmus_C57-6J_LVR_FLT_C_Rep3_M27', 'Mmus_C57-6J_LVR_FLT_C_Rep4_M28', 'Mmus_C57-6J_LVR_FLT_C_Rep5_M30'
-    ]
+    assert list(dataset.samples.keys()) == glds48_sample_names
 
     # check expected loaded components [raw directory]
-    assert len(list(ds.dataset.samples['Mmus_C57-6J_LVR_GC_I_Rep1_M31'].rawReads.multiQCDir.path.iterdir())) == 2
+    for sample in ds.dataset.samples.values():
+        assert len(list(sample.rawReads.multiQCDir.path.iterdir())) == 2
+        assert len(list(sample.trimReads.multiQCDir.path.iterdir())) == 2
 
 
-def test_from_bulk_rnaseq_raw_dir_paired(caplog):
+def test_bulk_rnaseq_STAGE01_paired(caplog, glds194_test_dir):
     """ Tests loader for state after demultiplexing for single end study """
-    target_data_dir = TEST_DIR / "GLDS-207_PostDemultiplex"
 
     caplog.set_level(0)
-    ds = load_BulkRNASeq_STAGE_00(
-        target_data_dir,
-        dataSystem_name = "GLDS-207"
+    ds = load_BulkRNASeq_STAGE_01(
+        *load_BulkRNASeq_STAGE_00(
+            glds194_test_dir, dataSystem_name="GLDS-194", stack=True
+        )
     )
 
     # pull dataset
-    dataset = ds.datasets["GLDS-207__BulkRNASeq"]
+    dataset = ds.datasets["GLDS-194__BulkRNASeq"]
 
     assert list(dataset.samples.keys()) == [
-        'KCNQ370_Female_Ground_Control-01', 'KCNQ370_Female_Ground_Control-02', 'KCNQ370_Female_Ground_Control-03', 'KCNQ370_Female_Ground_Control-04', 'KCNQ97_Female_Ground_Control-05', 'KCNQ97_Female_Ground_Control-06', 'KCNQ97_Female_Ground_Control-07', 'KCNQ97_Female_Ground_Control-08', 'Sei_ts1-Female_Ground_Control-09', 'Sei_ts1-Female_Ground_Control-10', 'Sei_ts1-Female_Ground_Control-11', 'Sei_ts1-Female_Ground_Control-12', 'CS-Female_Ground_Control-13', 'CS-Female_Ground_Control-14', 'CS-Female_Ground_Control-15', 'CS-Female_Ground_Control-16', 'KCNQ370_Female_Space_Flown-17', 'KCNQ370_Female_Space_Flown-18', 'KCNQ370_Female_Space_Flown-19', 'KCNQ370_Female_Space_Flown-20', 'KCNQ97_Female_Space_Flown-21', 'KCNQ97_Female_Space_Flown-22', 'KCNQ97_Female_Space_Flown-23', 'KCNQ97_Female_Space_Flown-24', 'Sei_ts1-Female_Space_Flown-25', 'Sei_ts1-Female_Space_Flown-26', 'Sei_ts1-Female_Space_Flown-27', 'Sei_ts1-Female_Space_Flown-28', 'CS-Female_Space_Flown-29', 'CS-Female_Space_Flown-30', 'CS-Female_Space_Flown-31', 'CS-Female_Space_Flown-32'
+        "Mmus_BAL-TAL_LRTN_BSL_Rep1_B7",
+        "Mmus_BAL-TAL_RRTN_BSL_Rep2_B8",
+        "Mmus_BAL-TAL_RRTN_BSL_Rep3_B9",
+        "Mmus_BAL-TAL_RRTN_BSL_Rep4_B10",
+        "Mmus_BAL-TAL_LRTN_GC_Rep1_G6",
+        "Mmus_BAL-TAL_LRTN_GC_Rep2_G8",
+        "Mmus_BAL-TAL_LRTN_GC_Rep3_G9",
+        "Mmus_BAL-TAL_RRTN_GC_Rep4_G10",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep1_F6",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep2_F7",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep3_F8",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep4_F9",
+        "Mmus_BAL-TAL_LRTN_FLT_Rep5_F10",
     ]
 
     # check expected loaded components [raw directory]
-    for sample_name in ds.dataset.samples:
-        assert len(list(ds.dataset.samples[sample_name].rawForwardReads.multiQCDir.path.iterdir())) == 2
-        assert len(list(ds.dataset.samples[sample_name].rawReverseReads.multiQCDir.path.iterdir())) == 2
+    for sample in ds.dataset.samples.values():
+        assert len(list(sample.rawForwardReads.multiQCDir.path.iterdir())) == 2
+        assert len(list(sample.rawReverseReads.multiQCDir.path.iterdir())) == 2
+        assert len(list(sample.trimForwardReads.multiQCDir.path.iterdir())) == 2
+        assert len(list(sample.trimReverseReads.multiQCDir.path.iterdir())) == 2
 
-def test_from_bulk_rnaseq_STAGE_01_paired(caplog):
-    """ Tests loader for state after demultiplexing for single end study """
-    target_data_dir = TEST_DIR / "GLDS-207_TruncatedProcessed/"
-
-    caplog.set_level(0)
-    ds = load_BulkRNASeq_STAGE_01(*load_BulkRNASeq_STAGE_00(
-        target_data_dir,
-        dataSystem_name = "GLDS-207",
-        stack = True
-    ))
-
-    # pull dataset
-    dataset = ds.datasets["GLDS-207__BulkRNASeq"]
-
-    assert list(dataset.samples.keys()) == [
-        'KCNQ370_Female_Ground_Control-01', 'KCNQ370_Female_Ground_Control-02', 'KCNQ370_Female_Ground_Control-03', 'KCNQ370_Female_Ground_Control-04', 'KCNQ97_Female_Ground_Control-05', 'KCNQ97_Female_Ground_Control-06', 'KCNQ97_Female_Ground_Control-07', 'KCNQ97_Female_Ground_Control-08', 'Sei_ts1-Female_Ground_Control-09', 'Sei_ts1-Female_Ground_Control-10', 'Sei_ts1-Female_Ground_Control-11', 'Sei_ts1-Female_Ground_Control-12', 'CS-Female_Ground_Control-13', 'CS-Female_Ground_Control-14', 'CS-Female_Ground_Control-15', 'CS-Female_Ground_Control-16', 'KCNQ370_Female_Space_Flown-17', 'KCNQ370_Female_Space_Flown-18', 'KCNQ370_Female_Space_Flown-19', 'KCNQ370_Female_Space_Flown-20', 'KCNQ97_Female_Space_Flown-21', 'KCNQ97_Female_Space_Flown-22', 'KCNQ97_Female_Space_Flown-23', 'KCNQ97_Female_Space_Flown-24', 'Sei_ts1-Female_Space_Flown-25', 'Sei_ts1-Female_Space_Flown-26', 'Sei_ts1-Female_Space_Flown-27', 'Sei_ts1-Female_Space_Flown-28', 'CS-Female_Space_Flown-29', 'CS-Female_Space_Flown-30', 'CS-Female_Space_Flown-31', 'CS-Female_Space_Flown-32'
-    ]
-
-    # check expected loaded components [raw directory]
-    for sample_name in ds.dataset.samples:
-        assert len(list(ds.dataset.samples[sample_name].rawForwardReads.multiQCDir.path.iterdir())) == 2
-        assert len(list(ds.dataset.samples[sample_name].rawReverseReads.multiQCDir.path.iterdir())) == 2
-        assert len(list(ds.dataset.samples[sample_name].trimForwardReads.multiQCDir.path.iterdir())) == 2
-        assert len(list(ds.dataset.samples[sample_name].trimReverseReads.multiQCDir.path.iterdir())) == 2
-
-def test_bulk_rnaseq_STAGE_01_single(caplog):
-    """ Tests loader for state after demultiplexing for single end study """
-    target_data_dir = TEST_DIR / "GLDS-48_TruncatedProcessed"
-
-    caplog.set_level(0)
-    ds = load_BulkRNASeq_STAGE_01(*load_BulkRNASeq_STAGE_00(
-        target_data_dir,
-        dataSystem_name = "GLDS-48",
-        stack = True
-    ))
-
-    # pull dataset
-    dataset = ds.datasets["GLDS-48__BulkRNASeq"]
-
-    assert list(dataset.samples.keys()) == [
-        'Mmus_C57-6J_LVR_GC_I_Rep1_M31', 'Mmus_C57-6J_LVR_GC_I_Rep2_M32', 'Mmus_C57-6J_LVR_FLT_I_Rep1_M21', 'Mmus_C57-6J_LVR_FLT_I_Rep2_M22', 'Mmus_C57-6J_LVR_GC_C_Rep1_M36', 'Mmus_C57-6J_LVR_GC_C_Rep2_M37', 'Mmus_C57-6J_LVR_GC_C_Rep3_M38', 'Mmus_C57-6J_LVR_GC_C_Rep4_M39', 'Mmus_C57-6J_LVR_GC_C_Rep5_M40', 'Mmus_C57-6J_LVR_FLT_C_Rep1_M25', 'Mmus_C57-6J_LVR_FLT_C_Rep2_M26', 'Mmus_C57-6J_LVR_FLT_C_Rep3_M27', 'Mmus_C57-6J_LVR_FLT_C_Rep4_M28', 'Mmus_C57-6J_LVR_FLT_C_Rep5_M30'
-    ]
-
-    # check expected loaded components [raw directory]
-    assert len(list(ds.dataset.samples['Mmus_C57-6J_LVR_GC_I_Rep1_M31'].rawReads.multiQCDir.path.iterdir())) == 2
-    assert len(list(ds.dataset.samples['Mmus_C57-6J_LVR_GC_I_Rep1_M31'].trimReads.multiQCDir.path.iterdir())) == 2
