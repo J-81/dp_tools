60ae50daa01bdd5cfdd2fa670f88c26659f179a7
J-81 <jonathan.d.oribello@gmail.com>
2022-03-21 18:54:14 -0700

    Added tests for new check

--- tests/checks/bulkRNASeq/test_bulkRNASeq_dataset_checks.py
+++ tests/checks/bulkRNASeq/test_bulkRNASeq_dataset_checks.py
@@ -1,19 +1,76 @@
 """ Tests for specific validation checks """
 from unittest.mock import MagicMock
 from pytest import MonkeyPatch
+import pytest
+
+from dp_tools.bulkRNASeq.checks import DATASET_METADATA_0001
 
 
-def test_DATASET_METADATA_0001_paired():
+@pytest.fixture(scope="module")
+def test_DATASET_METADATA_0001():
+    return DATASET_METADATA_0001()
+
+
+@pytest.fixture(scope="module")
+def test_DATASET_RAWREADS_0001():
+    return DATASET_RAWREADS_0001()
+
+
+def test_DATASET_METADATA_0001_paired(
+    glds194_dataSystem_STAGE00, test_DATASET_METADATA_0001
+):
+    dataset = glds194_dataSystem_STAGE00.dataset
+    testCheck = test_DATASET_METADATA_0001
+
+    # expected GREEN
+    flag = testCheck.validate(dataset)
+    assert flag.code.name == "GREEN"
+    assert (
+        flag.message
+        == "All expected metadata is accessible and populated. {'paired_end': True, 'has_ercc': True}"
+    )
+
+    # expected HALT1
+    with MonkeyPatch.context() as m:
+        m.setattr(dataset.metadata, "has_ercc", None)
+        flag = testCheck.validate(dataset)
+        assert flag.code.name == "HALT1"
+        assert flag.message == "Missing expected metadata fields: ['has_ercc']"
+
+
+def test_DATASET_METADATA_0001_single(
+    glds48_dataSystem_STAGE00, test_DATASET_METADATA_0001
+):
+    dataset = glds48_dataSystem_STAGE00.dataset
+    testCheck = test_DATASET_METADATA_0001
+
+    # expected GREEN
+    flag = testCheck.validate(dataset)
+    assert flag.code.name == "GREEN"
+    assert (
+        flag.message
+        == "All expected metadata is accessible and populated. {'paired_end': False, 'has_ercc': False}"
+    )
+
+    # expected HALT1
+    with MonkeyPatch.context() as m:
+        m.setattr(dataset.metadata, "has_ercc", None)
+        flag = testCheck.validate(dataset)
+        assert flag.code.name == "HALT1"
+        assert flag.message == "Missing expected metadata fields: ['has_ercc']"
+
+
+def test_DATASET_RAWREADS_0001_paired(
+    glds194_dataSystem_STAGE00, test_DATASET_RAWREADS_0001_paired
+):
     raise NotImplementedError
 
-def test_DATASET_METADATA_0001_single():
+
+def test_DATASET_RAWREADS_0001_single(
+    glds194_dataSystem_STAGE00, test_DATASET_RAWREADS_0001_paired
+):
     raise NotImplementedError
 
-def test_DATASET_RAWREADS_0001_paired():
-    raise NotImplementedError
-
-def test_DATASET_RAWREADS_0001_single():
-    raise NotImplementedError
 
 def test_DATASET_TRIMREADS_0001_paired():
     raise NotImplementedError
@@ -22,26 +79,34 @@ def test_DATASET_TRIMREADS_0001_paired():
 def test_DATASET_TRIMREADS_0001_single():
     raise NotImplementedError
 
+
 def test_DATASET_GENOMEALIGNMENTS_0001_paired():
     raise NotImplementedError
+
 
 def test_DATASET_GENOMEALIGNMENTS_0001_single():
     raise NotImplementedError
 
+
 def test_DATASET_RSEQCANALYSIS_0001_paired():
     raise NotImplementedError
+
 
 def test_DATASET_RSEQCANALYSIS_0001_single():
     raise NotImplementedError
 
+
 def test_DATASET_GENECOUNTS_0001_paired():
     raise NotImplementedError
+
 
 def test_DATASET_GENECOUNTS_0001_single():
     raise NotImplementedError
 
+
 def test_DATASET_DIFFERENTIALGENEEXPRESSION_0001_paired():
     raise NotImplementedError
+
 
 def test_DATASET_DIFFERENTIALGENEEXPRESSION_0001_single():
     raise NotImplementedError
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -35,32 +35,11 @@ def test_bulkRNASeq_STAGE00_validation_paired(caplog, glds194_dataSystem_STAGE00
     assert isinstance(vv_protocol.flags["sample"], dict)
     assert isinstance(vv_protocol.flags["component"], dict)
 
-    warn_msgs = [r.message for r in caplog.records if r.levelname == "WARNING"]
-    # assert not warn_msgs  # no warnings expected
-
-    # first, run a check with an unhandled developer exception
-    # with MonkeyPatch.context() as m:
-    #     from dp_tools.bulkRNASeq.checks import SAMPLE_RAWREADS_0001
-    #     m.setattr(SAMPLE_RAWREADS_0001, "validate_func", lambda self, sample: 1/0)
-    #     vv_protocol.validate_all()
-    #     sample, testcase_flags = list(vv_protocol.flags["sample"].items())[0]
-    #     testcase_flag = testcase_flags[0]
-    #     assert testcase_flag.check.id == "SAMPLE_RAWREADS_0001"
-    #     assert testcase_flag.code.value == 91
-    #     assert testcase_flag.message.startswith("An unhandled exception occured in ")
-    #     assert isinstance(sample, BulkRNASeqSample)
-
     # second, run with full validation
     with caplog.at_level(CAPLEVEL):
         caplog.clear()
         with MonkeyPatch.context() as m:
             vv_protocol.validate_all()
-            sample, testcase_flags = list(vv_protocol.flags["sample"].items())[0]
-            testcase_flag = testcase_flags[0]
-            assert testcase_flag.check.id == "SAMPLE_RAWREADS_0001"
-            assert testcase_flag.code.value == 20
-            assert testcase_flag.message == "All expected raw read files present"
-            assert isinstance(sample, BulkRNASeqSample)
             df = vv_protocol.flags_to_df()
 
             df_verbose = vv_protocol.flags_to_df(schema="verbose")
@@ -70,11 +49,11 @@ def test_bulkRNASeq_STAGE00_validation_paired(caplog, glds194_dataSystem_STAGE00
 
             # check if appropriate number of flags are raised
             # Currently:
-            #   Dataset check : 1
+            #   Dataset check : 2
             #   Sample check : 1 per sample
             #   Component checks :
             #       Reads : 1 per component
-            assert len(df) == 1 + len(ds.dataset.samples) * 3
+            assert len(df) == 2 + len(ds.dataset.samples) * 3
 
 
 def test_bulkRNASeq_STAGE00_validation_single(caplog, glds48_dataSystem_STAGE00):
@@ -85,54 +64,22 @@ def test_bulkRNASeq_STAGE00_validation_single(caplog, glds48_dataSystem_STAGE00)
     ds = glds48_dataSystem_STAGE00
     vv_protocol = BulkRNASeq_VVProtocol_RawData(dataset=ds.dataset)
 
-    with caplog.at_level(CAPLEVEL):
+    with MonkeyPatch.context() as m:
         vv_protocol.validate_all()
+        df = vv_protocol.flags_to_df()
 
-    assert isinstance(vv_protocol.flags["dataset"], dict)
-    assert isinstance(vv_protocol.flags["sample"], dict)
-    assert isinstance(vv_protocol.flags["component"], dict)
+        df_verbose = vv_protocol.flags_to_df(schema="verbose")
 
-    warn_msgs = [r.message for r in caplog.records if r.levelname == "WARNING"]
-    # assert not warn_msgs  # no warnings expected
+        # assert that no failing flags were raised
+        assert df["flag_code"].max() == 20
 
-    # first, run a check with an unhandled developer exception
-    # with MonkeyPatch.context() as m:
-    #     from dp_tools.bulkRNASeq.checks import SAMPLE_RAWREADS_0001
-
-    #     m.setattr(SAMPLE_RAWREADS_0001, "validate_func", lambda: 1 / 0)
-    #     vv_protocol.validate_all()
-    #     sample, testcase_flags = list(vv_protocol.flags["sample"].items())[0]
-    #     testcase_flag = testcase_flags[0]
-    #     assert testcase_flag.check.id == "SAMPLE_RAWREADS_0001"
-    #     assert testcase_flag.code.value == 91
-    #     assert testcase_flag.message.startswith("An unhandled exception occured in ")
-    #     assert isinstance(sample, BulkRNASeqSample)
-
-    # second, run with full validation
-    with caplog.at_level(CAPLEVEL):
-        caplog.clear()
-        with MonkeyPatch.context() as m:
-            vv_protocol.validate_all()
-            sample, testcase_flags = list(vv_protocol.flags["sample"].items())[0]
-            testcase_flag = testcase_flags[0]
-            assert testcase_flag.check.id == "SAMPLE_RAWREADS_0001"
-            assert testcase_flag.code.value == 20
-            assert testcase_flag.message == "All expected raw read files present"
-            assert isinstance(sample, BulkRNASeqSample)
-            df = vv_protocol.flags_to_df()
-
-            df_verbose = vv_protocol.flags_to_df(schema="verbose")
-
-            # assert that no failing flags were raised
-            assert df["flag_code"].max() == 20
-
-            # check if appropriate number of flags are raised
-            # Currently:
-            #   Dataset check : 1
-            #   Sample check : 1 per sample
-            #   Component checks
-            #       Reads : 1 per component (1 per sample)
-            assert len(df) == 1 + len(ds.dataset.samples) * 2
+        # check if appropriate number of flags are raised
+        # Currently:
+        #   Dataset check : 2
+        #   Sample check : 1 per sample
+        #   Component checks
+        #       Reads : 1 per component (1 per sample)
+        assert len(df) == 2 + len(ds.dataset.samples) * 2
 
 
 def test_bulkRNASeq_STAGE01_validation_paired(caplog, glds194_dataSystem_STAGE00):
