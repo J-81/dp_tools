23dad456d41557a32801c9f30b5fd659329b251b
J-81 <jonathan.d.oribello@gmail.com>
2022-03-22 20:00:24 -0700

    In progress stub for new check: DATASET_RSEQCANALYSIS_0001

--- dp_tools/bulkRNASeq/checks.py
+++ dp_tools/bulkRNASeq/checks.py
@@ -525,3 +525,79 @@ class DATASET_GENOMEALIGNMENTS_0001(Check):
             },
         )
 
+
+class DATASET_RSEQCANALYSIS_0001(Check):
+    config = {
+        "metrics": [],
+        "middle": MIDDLE.median,
+        "yellow_standard_deviation_threshold": 2,
+        "red_standard_deviation_threshold": 4,
+    }
+    description = (
+        "Check that the rseqc analysis stats (source from the rseqc logs) have no outliers among samples "
+        "for the following metrics: {metrics}. "
+        "Yellow Flagged Outliers are defined as a being {yellow_standard_deviation_threshold} - {red_standard_deviation_threshold} standard "
+        "deviations away from the {middle.name}. "
+        "Red Flagged Outliers are defined as a being {red_standard_deviation_threshold}+ standard "
+        "deviations away from the {middle.name}. "
+    )
+    flag_desc = {
+        FlagCode.GREEN: "No rseqc analysis metric outliers detected for {metrics}",
+        FlagCode.YELLOW1: "Outliers detected as follows (values are rounded number of standard deviations from middle): {formatted_outliers}",
+        FlagCode.RED1: "Outliers detected as follows (values are rounded number of standard deviations from middle): {formatted_outliers}",
+    }
+
+    def validate_func(self: Check, dataset: TemplateDataset) -> Flag:
+        code = FlagCode.GREEN
+
+        # pull variables from config
+        metrics = self.config["metrics"]
+        middle = self.config["middle"]
+        yellow_threshold = self.config["yellow_standard_deviation_threshold"]
+        red_threshold = self.config["red_standard_deviation_threshold"]
+
+        # init trackers for issues
+        outliers: DefaultDict[str, Dict[str, float]] = defaultdict(dict)
+
+        # determine reads components in samples
+        targetComponents = ["rSeQCAnalysis"]
+
+        # iterate through metrics (here all pulled from FastQC general stats)
+        for targetComponent in targetComponents:
+            for metric in metrics:
+                sampleToMetric: Dict[str, float] = {
+                    s.name: getattr(s, targetComponent).mqcData["RSeQC"][
+                        "General_Stats"
+                    ][metric]
+                    for s in dataset.samples.values()
+                }
+
+                # yellow level outliers
+                if outliersForThisMetric := identify_outliers(
+                    sampleToMetric,
+                    standard_deviation_threshold=yellow_threshold,
+                    middle=middle,
+                ):
+                    if code < FlagCode.YELLOW1:
+                        code = FlagCode.YELLOW1
+                    outliers[metric] = outliers[metric] | outliersForThisMetric
+
+                # red level outliers
+                if outliersForThisMetric := identify_outliers(
+                    sampleToMetric,
+                    standard_deviation_threshold=red_threshold,
+                    middle=middle,
+                ):
+                    if code < FlagCode.RED1:
+                        code = FlagCode.RED1
+                    outliers[metric] = outliers[metric] | outliersForThisMetric
+
+        return Flag(
+            code=code,
+            check=self,
+            message_args={
+                "outliers": outliers,
+                "metrics": metrics,
+                "formatted_outliers": pformat(outliers, formatfloat),
+            },
+        )
