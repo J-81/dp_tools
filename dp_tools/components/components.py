6f70864198ac058b1f47ce9437ebd8669e866fac
J-81 <jonathan.d.oribello@gmail.com>
2022-03-31 13:01:17 -0700

    Added to Metadata: - contrasts and factorGroups - Includes data formatting

--- dp_tools/components/components.py
+++ dp_tools/components/components.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+import itertools
 from pathlib import Path
 from typing import Dict, List, OrderedDict, Union
 
@@ -74,6 +75,90 @@ class BulkRNASeqMetadataComponent(TemplateComponent):
             )  # explicit conversion from numpy bool to standard bool
 
         strict_type_checks(self)
+
+    @property
+    def factor_groups(self) -> Dict[str, str]:
+        """ Returns sample wise dictionary of sample name to concatenated factor group.
+
+        The concatenated group will be consistent with the BulkRNASeq Deseq2 script formatting for constrasts.
+        This means:
+         - replacing within factor 'spaces' with periods
+         - wrapping the factor in quotes
+         - joining factors into factor groups with ' & '
+         - finally joining factors groups to derive pairwise-contrasts with 'v'
+        
+        """
+        if getattr(self, "_factor_groups", None) == None:  # uncached
+            if self.runsheet:
+                # read as dataframe
+                df = pd.read_csv(self.runsheet.path).set_index("sample_name")
+
+                # find factors columns
+                factor_cols = [c for c in df.columns if c.startswith("Factor Value[")]
+
+                # create concatenated group column
+                df["FactorGroup"] = (
+                    "(" + df[factor_cols].agg(" & ".join, axis="columns") + ")"
+                )
+
+                self._factor_groups = df["FactorGroup"].to_dict()
+
+            else:
+                raise ValueError(
+                    "No source of experimental factors available. Supported sources: 'runsheet'"
+                )
+        return self._factor_groups
+
+    @property
+    def contrasts(self) -> pd.DataFrame:
+        """ Returns sample wise dictionary of sample name to concatenated factor group.
+
+        Creates a constrast dataframe in the same fashion as the DESEQ2 contrasts table:
+        - determine all permutations of existing factor groups
+        - format with header for each permutation in format: {factorGroup1}v{factorGroup2}
+        - format with two rows for each column in format: 
+          - r1: factorGroup1.replace(' & ','...').replace(' ','.').replace('(',"").replace(')',"") 
+          - r2: factorGroup1.replace(' & ','...').replace(' ','.').replace('(',"").replace(')',"")
+        """
+        if getattr(self, "_contrasts_cached", None) == None:  # uncached
+            # determine all permutations of existing factor groups
+            permutations = list(
+                itertools.permutations(set(self.factor_groups.values()), 2)
+            )
+
+            # format with header for each permutation
+            headers = ["v".join(permute) for permute in permutations]
+
+            # format with two rows for each column
+            row1s = [
+                permute[0]
+                .replace(" & ", "...")
+                .replace(" ", ".")
+                .replace("(", "")
+                .replace(")", "")
+                for permute in permutations
+            ]
+            row2s = [
+                permute[1]
+                .replace(" & ", "...")
+                .replace(" ", ".")
+                .replace("(", "")
+                .replace(")", "")
+                for permute in permutations
+            ]
+
+            # format as dataframe
+            self._contrasts = pd.DataFrame(
+                {
+                    header: [row1, row2]
+                    for header, row1, row2 in zip(headers, row1s, row2s)
+                }
+            )
+            # use this for cache check as:
+            # 'ValueError: The truth value of a DataFrame is ambiguous. Use a.empty, a.bool(), a.item(), a.any() or a.all().'
+            self._contrasts_cached = True
+
+        return self._contrasts
 
 
 @dataclass(eq=False)
