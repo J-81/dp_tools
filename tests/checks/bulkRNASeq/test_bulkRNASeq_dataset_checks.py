7bf896da09cb79a84a4378873e28b8d035ec5bf2
J-81 <jonathan.d.oribello@gmail.com>
2022-03-31 13:03:14 -0700

    Tests for DATASET_DIFFERENTIALGENEEXPRESSION_0001  setup

--- tests/checks/bulkRNASeq/test_bulkRNASeq_dataset_checks.py
+++ tests/checks/bulkRNASeq/test_bulkRNASeq_dataset_checks.py
@@ -4,6 +4,8 @@ from pytest import MonkeyPatch
 import pytest
 
 from dp_tools.bulkRNASeq.checks import (
+    DATASET_DIFFERENTIALGENEEXPRESSION_0001,
+    DATASET_GENECOUNTS_0001,
     DATASET_GENOMEALIGNMENTS_0001,
     DATASET_METADATA_0001,
     DATASET_RAWREADS_0001,
@@ -47,6 +49,12 @@ def test_DATASET_RSEQCANALYSIS_0001():
 @pytest.fixture(scope="module")
 def test_DATASET_GENECOUNTS_0001():
     return DATASET_GENECOUNTS_0001()
+
+
+@pytest.fixture(scope="module")
+def test_DATASET_DIFFERENTIALGENEEXPRESSION_0001():
+    return DATASET_DIFFERENTIALGENEEXPRESSION_0001()
+
 
 def test_DATASET_METADATA_0001_paired(
     glds194_dataSystem_STAGE00, test_DATASET_METADATA_0001
@@ -234,13 +242,24 @@ def test_DATASET_GENECOUNTS_0001_single(
     assert flag.maxCode.name == "YELLOW1"
 
 
-def test_DATASET_GENECOUNTS_0001_single():
-    raise NotImplementedError
+def test_DATASET_DIFFERENTIALGENEEXPRESSION_0001_paired(
+    glds194_dataSystem_STAGE04, test_DATASET_DIFFERENTIALGENEEXPRESSION_0001
+):
+    dataset = glds194_dataSystem_STAGE04.dataset
+    testCheck = test_DATASET_DIFFERENTIALGENEEXPRESSION_0001
+
+    flag = testCheck.validate(dataset)
+    assert flag.maxCode.name == "YELLOW1"
+    assert [c.name for c in flag.codes] == ["YELLOW1"]
 
 
-def test_DATASET_DIFFERENTIALGENEEXPRESSION_0001_paired():
-    raise NotImplementedError
+def test_DATASET_DIFFERENTIALGENEEXPRESSION_0001_single(
+    glds48_dataSystem_STAGE04, test_DATASET_DIFFERENTIALGENEEXPRESSION_0001
+):
+    dataset = glds48_dataSystem_STAGE04.dataset
+    testCheck = test_DATASET_DIFFERENTIALGENEEXPRESSION_0001
 
+    # expected YELLOW1
+    flag = testCheck.validate(dataset)
+    assert flag.maxCode.name == "YELLOW1"
 
-def test_DATASET_DIFFERENTIALGENEEXPRESSION_0001_single():
-    raise NotImplementedError
