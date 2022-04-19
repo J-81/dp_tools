3ab50a45f85256b17f276d29d42be747c0fe1c5c
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-04-19 13:45:29 -0700

    Added in file and url mapping functionality.

---
+++ dp_tools/glds_api/commons.py
@@ -0,0 +1,35 @@
+import datetime
+import functools
+from json import loads
+from urllib.request import urlopen
+import logging
+
+log = logging.getLogger(__name__)
+# Function to pull metadata zip from GeneLab
+# Credit to Kirill Grigorev
+GENELAB_ROOT = "https://genelab-data.ndc.nasa.gov"
+GLDS_URL_PREFIX = GENELAB_ROOT + "/genelab/data/study/data/"
+FILELISTINGS_URL_PREFIX = GENELAB_ROOT + "/genelab/data/study/filelistings/"
+ISA_ZIP_REGEX = r".*_metadata_.*[_-]ISA\.zip$"
+
+
+def read_json(url):
+    with urlopen(url) as response:
+        return loads(response.read().decode())
+
+
+@functools.cache
+def get_glds_filelisting_json(accession: str) -> tuple[dict, str]:
+    """Return filelisting json accession number"""
+    log.debug(f"Fetching filelisting JSON for '{accession}'")
+    # extract file urls
+    glds_json = read_json(GLDS_URL_PREFIX + accession)
+    try:
+        _id = glds_json[0]["_id"]
+    except (AssertionError, TypeError, KeyError, IndexError):
+        raise ValueError("Malformed JSON?")
+    file_list_url = FILELISTINGS_URL_PREFIX + _id
+    file_listing_json = read_json(file_list_url)
+    version = glds_json[0]["version"]
+    log.info(f"Retrieved file listing json. URL: '{file_list_url}' Version: '{version}' Access Date: '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M %Z')}'")
+    return file_listing_json
---
+++ dp_tools/glds_api/files.py
@@ -0,0 +1,48 @@
+""" Module for finding file urls as hosted on the GLDS repository """
+import functools
+from typing import Union
+from urllib.parse import quote
+from dp_tools.core.entity_model import DataFile, TemplateDataset, TemplateSample
+from dp_tools.glds_api.commons import GENELAB_ROOT, get_glds_filelisting_json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+
+def get_valid_url(fn: str, version: str) -> str:
+    return f"{GENELAB_ROOT}/genelab/static/media/dataset/{quote(fn)}?version={version}"
+
+
+def reformat_filelisting(original_json: dict) -> dict:
+    """Reformats the original file listing
+
+    :param original_json: _description_
+    :type original_json: dict
+    :return: _description_
+    :rtype: dict
+    """
+    return {
+        data["file_name"]: {"url": get_valid_url(data["file_name"], data["version"])}
+        for data in original_json
+    }
+
+
+@functools.cache
+def get_urls(accession: str) -> dict[str, str]:
+    file_listing_json = get_glds_filelisting_json(accession)
+    return reformat_filelisting(file_listing_json)
+
+
+def associate_url_for_data_file(
+    entity: Union[TemplateDataset, TemplateSample], target_asset: DataFile
+) -> str:
+    match entity:
+        case TemplateDataset():
+            dataset = entity
+        case TemplateSample():
+            dataset = entity.dataset
+    urls = get_urls(accession=dataset.dataSystem.name)
+    glds_name = f"{dataset.config['ISA Meta']['Global file prefix'].format(datasystem=dataset.dataSystem.name)}{target_asset.path.name}"
+    target_asset.glds_url = urls[glds_name]["url"]
+    return urls[glds_name]["url"]
