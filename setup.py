09254183a8626ba8c8ffdb9036ea37d602634f9b
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-04-19 18:34:06 -0700

    Updated to tag 1.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name="dp_tools",
-    version="1.0.2",
+    version="1.0.3",
     description="Tooling for Data Processing Operations",
     author="Jonathan Oribello",
     author_email="jonathan.d.oribello@nasa.gov",
