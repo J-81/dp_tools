3cf44f0f549321e369d0ab5795164b3322f96c2f
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-04-29 15:29:10 -0700

    Added Schema schemas to config. These should be imported in tandem with config loading.

---
+++ dp_tools/config/schemas.py
@@ -0,0 +1,37 @@
+""" Schemas for validation 
+Uses Schema to allow usage of validation functions
+"""
+from schema import Schema, Optional
+
+runsheet = {
+    "bulkRNASeq": Schema(
+        {
+            str: {
+                "Original Sample Name": str,
+                "has_ERCC": bool,
+                "organism": str,
+                "paired_end": bool,
+                "read1_path": str,
+                Optional("read2_path"): str,
+                str: object,  # this is used to pass other columns, chiefly Factor Value ones
+            }
+        }
+    ),
+    "microarray": Schema(
+        {
+            str: {
+                "Original Sample Name": str,
+                "organism": str,
+                "Study Assay Measurement": str,
+                "Study Assay Technology Type": str,
+                "Study Assay Technology Platform": str,
+                "Source Name": str,
+                "Label": str,
+                "Hybridization Assay Name": str,
+                "Array Data File Name": str,
+                "Array Data File Path": str,
+                str: object,  # this is used to pass other columns, chiefly Factor Value ones
+            }
+        }
+    ),
+}
