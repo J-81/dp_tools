b3b30e57347fcd4f654e4f089822498b3c7ab235
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-05-16 16:46:18 -0700

    Added function checks for certain trickier to implement functions that required more testing.

---
+++ tests/checks/test_function_checks.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+from dp_tools.bulkRNASeq.checks import (
+    check_bam_file_integrity,
+    check_forward_and_reverse_reads_counts_match,
+)
+from dp_tools.core.check_model import FlagCode
+
+
+def test_check_forward_and_reverse_reads_counts_match(glds194_dataSystem_STAGE00):
+    dataset = glds194_dataSystem_STAGE00.dataset
+
+    for sample in dataset.samples.values():
+        res = check_forward_and_reverse_reads_counts_match(
+            fwd_reads=sample.rawForwardReads, rev_reads=sample.rawReverseReads
+        )
+        assert res["code"]
+        assert res["message"]
+
+
+def test_check_bam_file_integrity(glds48_dataSystem_STAGE04):
+    dataset = glds48_dataSystem_STAGE04.dataset
+
+    for sample in dataset.samples.values():
+        res = check_bam_file_integrity(
+            file=sample.genomeAlignments.alignedToTranscriptomeBam.path,
+            samtools_bin=Path(os.environ.get("SAMTOOLS_BIN")),
+        )
+        assert res["code"] == FlagCode.GREEN
+        assert res["message"]
+
+        res = check_bam_file_integrity(
+            file=sample.genomeAlignments.logFinal.path,
+            samtools_bin=Path(os.environ.get("SAMTOOLS_BIN")),
+        )
+
+        assert res["code"] == FlagCode.HALT1
+        assert res["message"]
