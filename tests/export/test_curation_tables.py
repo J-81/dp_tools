497150150cc3e99fff871671dd478bc5e81d6d6f
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-05-23 16:17:37 -0700

    Added paired end variant. Added shape based dataframe validation.

--- tests/export/test_curation_tables.py
+++ tests/export/test_curation_tables.py
@@ -1,7 +1,17 @@
 from dp_tools.core.post_processing import update_curation_tables
 
 
-def test_update_curation_tables(glds48_dataSystem_STAGE04):
-    update_curation_tables(
-        glds48_dataSystem_STAGE04.dataset, config=("bulkRNASeq", "0")
+def test_update_curation_tables_paired(glds194_dataSystem_STAGE04):
+    df = update_curation_tables(
+        glds194_dataSystem_STAGE04.dataset, config=("bulkRNASeq", "Latest")
     )
+
+    assert df.shape == (13, 64)
+
+
+def test_update_curation_tables_single(glds48_dataSystem_STAGE04):
+    df = update_curation_tables(
+        glds48_dataSystem_STAGE04.dataset, config=("bulkRNASeq", "Latest")
+    )
+
+    assert df.shape == (14, 55)
