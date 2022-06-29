11613d3cf0f0566ab5eff48098f763c09519236c
Kenny Tang <tangkenny37@gmail.com>
2022-06-29 11:51:21 -0700

    Created practice check functions

---
+++ dp_tools/microarray/checks.py
@@ -0,0 +1,25 @@
+def check_if_extensions_valid(file, valid_ext):
+    """ Description of the function
+    Description Line 2
+
+    :param file: Input raw data file
+    :type file: Path
+    :param valid_ext: Extensions that are allow for the raw data files
+    :type valid_ext: list[str]
+    :return: A required fields-only flag entry dictionary
+    :rtype: FlagEntry
+    """
+    pass # Does nothing (... is equivalent)
+
+def check_factor_values_in_runsheet():
+    """ Description of the function
+    Description Line 2
+
+    :param file: Input raw data file
+    :type file: Path
+    :param valid_ext: Extensions that are allow for the raw data files
+    :type valid_ext: list[str]
+    :return: A required fields-only flag entry dictionary
+    :rtype: FlagEntry
+    """
+    pass # Does nothing (... is equivalent)
