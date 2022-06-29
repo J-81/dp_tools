f022e0eff19263cdabdcb9e69a10b79120b7a927
jbutelet <jbutelet@gmail.com>
2022-06-29 11:51:14 -0700

    added new functions

---
+++ dp_tools/microarray/checks.py
@@ -0,0 +1,8 @@
+def check_if_valid_extensions(file, valid_ext):
+    """description
+    params (description + data type)
+    return (description + data type)"""
+    pass
+def check_factor():
+    """description"""
+    pass
