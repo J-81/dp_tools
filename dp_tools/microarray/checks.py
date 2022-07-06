910b74903366ec9387b85896485c9cdfac94b230
jbutelet <jbutelet@gmail.com>
2022-07-05 19:19:03 -0700

    factor value check with test

--- dp_tools/microarray/checks.py
+++ dp_tools/microarray/checks.py
@@ -1,13 +1,13 @@
 from enum import Flag
 from dp_tools.core.check_model import FlagCode, FlagEntry
 import pandas as pd
-#should I import data all in one place like the car in the demo?
 
-def check_factor_values_in_runsheet(runsheet_file) -> FlagEntry: 
-    """ Checks for Factor Value by iterating over column names
-    :param file: Runsheet File
-    :type file: Path
-    :return: flagcode and message
+def check_factor_values_in_runsheet(runsheet_file: str) -> FlagEntry: 
+    """Verifies inclusion of factor values in runsheet
+
+    :param runsheet_file: Path to runsheet file
+    :type runsheet_file: str
+    :return: determination if factor value was located
     :rtype: FlagEntry
     """
     runsheet = pd.read_csv(runsheet_file)
@@ -19,31 +19,5 @@ def check_factor_values_in_runsheet(runsheet_file) -> FlagEntry:
             message = f"Factor Value column detected in runsheet"
     return {"code": code, "message": message}
 
-
-# def check_file_exists(file: Path) -> FlagEntry:
-#     # check logic
-#     if file.is_file():
-#         code = FlagCode.GREEN
-#         message = f"File exists: {file.name} "
-#     else:
-#         code = FlagCode.HALT
-#         message = f"Missing file: {file.name} expected at {str(file)} "
-
-#     return {"code": code, "message": message}
-
-def test_check_factor_values_in_runsheet():
-    res = check_factor_values_in_runsheet(<filename>)
-    assert res["code"] == FlagCode.Green
-    assert res["message"]
-
-# def main():
-#     check_factor_values_in_runsheet(r"C:\Users\linde\rmarkdown\affy_processing\data_runsheets\GLDS_6_runsheet.csv")
-# main()
-
-#Possible checks (took from bulkRNASeq)
-#if file exists 
-#check for non null entries
-#value content
-#   what should the values look like?
 
     
