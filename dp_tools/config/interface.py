b82ddd7a8285935261c12ccec6e116e981ea58f7
Jonathan Oribello <Jonathan.d.oribello@gmail.com>
2022-07-18 18:30:26 +0000

    Added functions for getting parts of the data assets section

--- dp_tools/config/interface.py
+++ dp_tools/config/interface.py
@@ -6,6 +6,7 @@ import yaml
 import logging
 log = logging.getLogger(__name__)
 from importlib.metadata import files
+import functools
 
 ConfigVersion = tuple[str, str]
 """ Denotes a specific prepackaged configuration file with (<assay>,<version>), e.g. ("bulkRNAseq","Latest") """
@@ -13,6 +14,7 @@ ConfigVersion = tuple[str, str]
 ConfigSelection = Union[ConfigVersion, Path]
 """ Specifies a configuration file, either prepackaged or by direct local path """
 
+@functools.cache # Allows repeated usage of this function without actually loading from file more than once
 def load_config(config: ConfigSelection) -> dict:
     """Load yaml configuration file. Allows loading from either:
       - A prepackaged configuration file using a tuple of ('config_type','config_version') (e.g. ('bulkRNASeq','Latest'), ('microarray','0'))
@@ -39,6 +41,16 @@ def load_config(config: ConfigSelection) -> dict:
 
     return conf_full
 
+def get_data_asset_keys(config: ConfigSelection) -> list[str]:
+    """
+    Get all data asset keys from the config
+
+    :param config: Configuration file to load
+    :type config: ConfigSelection
+    :return: list of keys
+    :rtype: list[str]
+    """
+    return list(load_config(config)['data assets'].keys())
 
 def get_data_asset_template(key: str, config: ConfigSelection) -> str:
     """Fetches the canonical processing output location for a given key
@@ -48,4 +60,13 @@ def get_data_asset_template(key: str, config: ConfigSelection) -> str:
     :return: A string denoting a path to the data asset. It may contain unfilled template sections which can be filled with the 'format' method
     :rtype: str
     """
-    ...
+    config_data_assets = load_config(config)['data assets']
+    query_config_data_asset = config_data_assets[key]['processed location']
+
+    # Retrieve full template path from list of paths
+    root_path = Path(query_config_data_asset[0])
+    full_path = None
+    for child_path in query_config_data_asset[1:]:
+        full_path = root_path / child_path if full_path is None else full_path / child_path
+    
+    return str(full_path)
--- tests/test_config_interface.py
+++ tests/test_config_interface.py
@@ -11,5 +11,10 @@ def test_load_config():
     #res = load_config(("microarray","0"))
     #assert res["data assets"]
 
+def test_get_data_asset_keys():
+    res = get_data_asset_keys(config = ("bulkRNASeq","Latest"))
+    assert 'raw reads fastq GZ' in res
+
 def test_get_data_asset_template():
-    ...
+    res = get_data_asset_template(key = "raw reads fastq GZ",config = ("bulkRNASeq","Latest"))
+    assert res == "00-RawData/Fastq/{sample}_raw.fastq.gz"
