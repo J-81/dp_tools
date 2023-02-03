f1da702b88e418721512df7e05000be5b8be48be
Jonathan Oribello <Jonathan.d.oribello@gmail.com>
2023-02-03 03:23:06 +0000

    fix: ensure plugins load regardless of location

--- dp_tools/plugin_api.py
+++ dp_tools/plugin_api.py
@@ -4,6 +4,9 @@ import sys
 import re
 from pathlib import Path
 
+import logging
+log = logging.getLogger(__name__)
+
 def load_all_plugins(plugin_dir: Path):
     sys.path.append(str(plugin_dir))
 
@@ -22,12 +25,10 @@ def load_all_plugins(plugin_dir: Path):
 
 def load_plugin(plugin_dir: Path):
     plugin_str = plugin_dir.name
-    sys.path.append(str(plugin_dir))
+    sys.path.append(str(plugin_dir.parent))
 
-    print ("DP TOOLS VALIDATION PLUGIN LOADER starting.  loading plugins:")
-    print(f"- {plugin_str}")
+    log.info(f"DP TOOLS VALIDATION PLUGIN LOADER starting.  loading plugins from '{plugin_dir}'")
     if re.search('^dp_tools__', plugin_str):
-        module_name = plugin_str
-        module = importlib.import_module(module_name)
+        module = importlib.import_module(plugin_str)
         return module
     raise ValueError(f"Could not find validate plugin in {plugin_dir}")
