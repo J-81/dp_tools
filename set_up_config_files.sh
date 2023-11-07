69f216370a4c7804f56ebda1f83634fa58269bb4
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2023-11-07 10:13:51 -0800

    copying from assets

--- set_up_config_files.sh
+++ set_up_config_files.sh
@@ -2,7 +2,7 @@ set -eux
 
 OUT_DIR=$1
 
-cp tests/assets/isa_config.yaml ${OUT_DIR}
-cp tests/assets/config.yaml ${OUT_DIR}/extraction_settings.yaml
+cp assets/isa_config.yaml ${OUT_DIR}
+cp assets/extraction_conf.yaml ${OUT_DIR}/extraction_settings.yaml
 
 # cp tests/assets/test.yaml ${OUT_DIR} # Disabled as test.yaml which contains user specified columns is not fully implemented
